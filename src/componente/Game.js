9edc909a0a016da4d72a9127157eadb00171edaf
Heitor <84941537+HeitorSantos-dev@users.noreply.github.com>
2024-04-19 20:07:27 -0300

    Add files via upload

---
+++ src/componente/Game.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class Game extends React.Component{
+    render(){
+        return(
+            <h1>Jogo da Velha:</h1>
+
+
+
+        );
+    }
+
+}
+export default Game;
